7a64be3e886012295085ab84e65f444a16d4d619
Mikhail Kabirov <mikhail.kbrv@gmail.com>
2015-09-19 11:46:46 +0500

    Текст из буфера обмена

---
+++ TextFromClipboard/TextFromClipboard/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TextFromClipboard
+{
+    class Program
+    {
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            String s = Clipboard.GetText();
+            string[] words = s.Split(new char[] {' ', ',', '.', '!', '?', ':', ';', '\n'}, StringSplitOptions.RemoveEmptyEntries);
+            words = words.OrderBy(x => x.ToLower()).Distinct().Take(1000).ToArray();
+            Console.WriteLine(string.Join("\n", words));
+            Console.ReadKey();            
+        }
+    }
+}
